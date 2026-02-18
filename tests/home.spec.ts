c8cc7b9f048a21b04d0bb52b7e8154584428a648
tomashermann <tomashermann10@gmail.com>
2026-02-18 13:47:15 +0100

    vymazal jsem kod z home souboru

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,8 +1,1 @@
 import test, { expect } from '../fixtures/basePages';
-
-test.describe('Home page tests', () => {
-    test.beforeEach(async ({ loginPage }) => {
-        await loginPage.goToLoginPage();
-        await loginPage.loginWithValidCredentials();
-    });
-});
