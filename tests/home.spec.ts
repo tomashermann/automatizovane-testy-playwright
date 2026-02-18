f0dea4d1cb2f894a5450e8e81b7f0d7497ec1d0b
tomashermann <tomashermann10@gmail.com>
2026-02-18 13:55:55 +0100

    pridal sem testy v home

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,1 +1,18 @@
 import test, { expect } from '../fixtures/basePages';
+
+test.describe('Home tests', () => {
+    test.beforeEach(async ({ loginPage }) => {
+        await loginPage.goToLoginPage();
+
+    });
+    
+    test('verify that the inventory page is displayed after successful login @Regression,@Smoke,@Sanity', async ({ page, loginPage, homePage }) => {
+        test.info().annotations.push({ 
+            type: 'Test', 
+            description: 'this test verifies that a user can log in with valid credentials and is redirected to the inventory page' 
+        });
+            await test.step('Login with valid credentials', async () => {
+                await loginPage.loginWithValidCredentials();
+        });
+    });
+}); 
