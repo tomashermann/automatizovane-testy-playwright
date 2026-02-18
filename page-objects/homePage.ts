1bed7dd1b05219562244ee1145e9393b1373fb65
tomashermann <tomashermann10@gmail.com>
2026-02-18 14:11:33 +0100

    pridaní funkcí do homepage

--- page-objects/homePage.ts
+++ page-objects/homePage.ts
@@ -18,4 +18,22 @@ export class HomePage {
         this.item5 = page.locator('#item_2_title_link');
         this.item6 = page.locator('#item_3_title_link');
     }
+    async clickOnItem1() {
+        await this.item1.click();
+    }
+    async clickOnItem2() {
+        await this.item2.click();
+    }
+    async clickOnItem3() {
+        await this.item3.click();
+    }
+    async clickOnItem4() {
+        await this.item4.click();
+    }
+    async clickOnItem5() {
+        await this.item5.click();
+    }
+    async clickOnItem6() {
+        await this.item6.click();
+    }   
 }
