71146a6f562aced3474aab0ba4d85645a0b2ad47
tomashermann <tomashermann10@gmail.com>
2026-02-18 23:43:06 +0100

    vytvoreni testu pro pridani a odebrani itemu z kosiku

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -56,26 +56,100 @@ import { SauceLabsPage } from '../page-objects/sauceLabsPage';
         await test.step('Add item to cart and verify cart badge update', async () => {
             await homePage.addToCartButton1.click();
             await expect(homePage.cartBadge).toHaveText('1');
+            await expect(homePage.removeButtonItem1).toBeVisible();
         });
         await test.step('Add another item to cart and verify cart badge update', async () => {
             await homePage.addToCartButton2.click();
             await expect(homePage.cartBadge).toHaveText('2');
+            await expect(homePage.removeButtonItem2).toBeVisible();
          });
         await test.step('Add third item to cart and verify cart badge update', async () => {
             await homePage.addToCartButton3.click();
             await expect(homePage.cartBadge).toHaveText('3');
+            await expect(homePage.removeButtonItem3).toBeVisible();
         });
         await test.step('Add fourth item to cart and verify cart badge update', async () => {
             await homePage.addToCartButton4.click();
             await expect(homePage.cartBadge).toHaveText('4');
+            await expect(homePage.removeButtonItem4).toBeVisible();
         });
         await test.step('Add fifth item to cart and verify cart badge update', async () => {
             await homePage.addToCartButton5.click();
             await expect(homePage.cartBadge).toHaveText('5');
+            await expect(homePage.removeButtonItem5).toBeVisible();
         });
         await test.step('Add sixth item to cart and verify cart badge update', async () => {
             await homePage.addToCartButton6.click();
             await expect(homePage.cartBadge).toHaveText('6');
+            await expect(homePage.removeButtonItem6).toBeVisible();
          });
+    });
+
+    test('Verifi removing item from cart and cart badge update @Regression,@Smoke', async ({ homePage }) => {
+        test.info().annotations.push({ 
+            type: 'Test', 
+            description: 'this test verifies that when an item is removed from the cart, the cart badge is updated with the correct number of items' 
+        });
+
+        await test.step('Add item to cart and verify cart badge update', async () => {
+            await homePage.addToCartButton1.click();
+            await expect(homePage.cartBadge).toHaveText('1');
+            await expect(homePage.removeButtonItem1).toBeVisible();
+        });
+        await test.step('Add another item to cart and verify cart badge update', async () => {
+            await homePage.addToCartButton2.click();
+            await expect(homePage.cartBadge).toHaveText('2');
+            await expect(homePage.removeButtonItem2).toBeVisible();
+         });
+        await test.step('Add third item to cart and verify cart badge update', async () => {
+            await homePage.addToCartButton3.click();
+            await expect(homePage.cartBadge).toHaveText('3');
+            await expect(homePage.removeButtonItem3).toBeVisible();
+        });
+        await test.step('Add fourth item to cart and verify cart badge update', async () => {
+            await homePage.addToCartButton4.click();
+            await expect(homePage.cartBadge).toHaveText('4');
+            await expect(homePage.removeButtonItem4).toBeVisible();
+        });
+        await test.step('Add fifth item to cart and verify cart badge update', async () => {
+            await homePage.addToCartButton5.click();
+            await expect(homePage.cartBadge).toHaveText('5');
+            await expect(homePage.removeButtonItem5).toBeVisible();
+        });
+        await test.step('Add sixth item to cart and verify cart badge update', async () => {
+            await homePage.addToCartButton6.click();
+            await expect(homePage.cartBadge).toHaveText('6');
+            await expect(homePage.removeButtonItem6).toBeVisible();
+         });
+         await test.step('Remove item from cart and verify cart badge update', async () => {
+            await homePage.removeButtonItem1.click();
+            await expect(homePage.cartBadge).toHaveText('5');
+            await expect(homePage.removeButtonItem1).toBeHidden();
+        });
+        await test.step('Remove another item from cart and verify cart badge update', async () => {
+            await homePage.removeButtonItem2.click();
+            await expect(homePage.cartBadge).toHaveText('4');
+            await expect(homePage.removeButtonItem2).toBeHidden();
+         });
+        await test.step('Remove third item from cart and verify cart badge update', async () => {
+            await homePage.removeButtonItem3.click();
+            await expect(homePage.cartBadge).toHaveText('3');
+            await expect(homePage.removeButtonItem3).toBeHidden();
+        });
+        await test.step('Remove fourth item from cart and verify cart badge update', async () => {
+            await homePage.removeButtonItem4.click();
+            await expect(homePage.cartBadge).toHaveText('2');
+            await expect(homePage.removeButtonItem4).toBeHidden();
+        });
+        await test.step('Remove fifth item from cart and verify cart badge update', async () => {
+            await homePage.removeButtonItem5.click();
+            await expect(homePage.cartBadge).toHaveText('1');
+            await expect(homePage.removeButtonItem5).toBeHidden();
+        });
+        await test.step('Remove sixth item from cart and verify cart badge update', async () => {
+            await homePage.removeButtonItem6.click();
+            await expect(homePage.shoppingCartBadge).toBeVisible();
+            await expect(homePage.removeButtonItem6).toBeHidden();
+        });
     });
 });
